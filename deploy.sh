94bce34e9c52cfb2228cccc01d6bfa90bf49f4d7
pNagi <nagisama.j@gmail.com>
2017-09-04 17:40:13 +0700

    :green_heart:

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@
 set -e
 
 SERVICE_NAME=$1
-cd $SERVICE_NAME && docker build -f Dockerfile -t gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1 .
+# cd $SERVICE_NAME && docker build -f Dockerfile -t gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1 .
 ${HOME}/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1
 # ${HOME}/google-cloud-sdk/bin/kubectl patch deployment $SERVICE_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"'$SERVICE_NAME'","image":"gcr.io/firsttry-178217/'$SERVICE_NAME':'$CIRCLE_SHA1'"}]}}}}'
