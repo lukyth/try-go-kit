8987341bf3b02b03855e16f44f8cea701e9861c2
pNagi <nagisama.j@gmail.com>
2017-09-06 15:54:06 +0700

    :bug: wrong image path

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ SERVICE_NAME=$1
 cd $SERVICE_NAME && docker build -f Dockerfile -t gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1 .
 ${HOME}/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1
 ${HOME}/google-cloud-sdk/bin/gcloud container images add-tag gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1 gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:latest
-${HOME}/google-cloud-sdk/bin/kubectl patch deployment $SERVICE_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"'$SERVICE_NAME'","image":"gcr.io/firsttry-178217/'$SERVICE_NAME':'$CIRCLE_SHA1'"}]}}}}'
+${HOME}/google-cloud-sdk/bin/kubectl patch deployment $SERVICE_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"'$SERVICE_NAME'","image":"gcr.io/firsttry-178217/try-go-kit/'$SERVICE_NAME':'$CIRCLE_SHA1'"}]}}}}'
