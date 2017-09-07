d311fffd02fe426eb7cf55e0adce7b8f70e5b917
pNagi <nagisama.j@gmail.com>
2017-09-07 16:07:44 +0700

    :green_heart: cache gcloud and declare const in deploy file

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,11 @@
 set -e
 
 SERVICE_NAME=$1
-cd $SERVICE_NAME && docker build -f Dockerfile -t gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1 .
-${HOME}/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1
-${HOME}/google-cloud-sdk/bin/gcloud container images add-tag gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1 gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:latest
-${HOME}/google-cloud-sdk/bin/kubectl set image deployment $SERVICE_NAME $SERVICE_NAME=gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1
+IMAGE_NAME=gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME
+IMAGE_NAME_WITH_TAG=$IMAGE_NAME:$CIRCLE_SHA1
+IMAGE_NAME_WITH_LATEST_TAG=$IMAGE_NAME:latest
+
+cd $SERVICE_NAME && docker build -f Dockerfile -t $IMAGE_NAME_WITH_TAG .
+${HOME}/google-cloud-sdk/bin/gcloud docker -- push $IMAGE_NAME_WITH_TAG
+${HOME}/google-cloud-sdk/bin/gcloud container images add-tag $IMAGE_NAME_WITH_TAG $IMAGE_NAME_WITH_LATEST_TAG
+${HOME}/google-cloud-sdk/bin/kubectl set image deployment $SERVICE_NAME $SERVICE_NAME=$IMAGE_NAME_WITH_TAG
