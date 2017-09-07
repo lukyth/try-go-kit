65daec87a046ebf7a4e4737fdd00f5a2447467f3
pNagi <nagisama.j@gmail.com>
2017-09-07 15:33:20 +0700

    :green_heart: gcloud set image

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,4 @@ SERVICE_NAME=$1
 cd $SERVICE_NAME && docker build -f Dockerfile -t gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1 .
 ${HOME}/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1
 ${HOME}/google-cloud-sdk/bin/gcloud container images add-tag gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1 gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:latest
+${HOME}/google-cloud-sdk/bin/kubectl set image deployment $SERVICE_NAME $SERVICE_NAME=gcr.io/firsttry-178217/try-go-kit/$SERVICE_NAME:$CIRCLE_SHA1
