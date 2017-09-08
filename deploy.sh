5699e0392ee479655fcc83cf94702200c5c84848
pNagi <nagisama.j@gmail.com>
2017-09-08 15:44:34 +0700

    :green_heart: fix path

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ IMAGE_NAME_WITH_TAG=$IMAGE_NAME:$CIRCLE_SHA1
 IMAGE_NAME_WITH_LATEST_TAG=$IMAGE_NAME:latest
 
 cd $SERVICE_NAME && docker build -f Dockerfile -t $IMAGE_NAME_WITH_TAG .
-${HOME}/google-cloud-sdk/bin/gcloud docker -- push $IMAGE_NAME_WITH_TAG
-${HOME}/google-cloud-sdk/bin/gcloud container images add-tag $IMAGE_NAME_WITH_TAG $IMAGE_NAME_WITH_LATEST_TAG
-${HOME}/google-cloud-sdk/bin/kubectl set image deployment $SERVICE_NAME $SERVICE_NAME=$IMAGE_NAME_WITH_TAG
+~/google-cloud-sdk/bin/gcloud docker -- push $IMAGE_NAME_WITH_TAG
+~/google-cloud-sdk/bin/gcloud container images add-tag $IMAGE_NAME_WITH_TAG $IMAGE_NAME_WITH_LATEST_TAG
+~/google-cloud-sdk/bin/kubectl set image deployment $SERVICE_NAME $SERVICE_NAME=$IMAGE_NAME_WITH_TAG
